532e2ecad1582f7ab5ea61dfcff3c25524ee11aa
Mingo20 <domingoguerra688@gmail.com>
2026-06-11 11:48:41 -0400

    ⚙️ Service Worker PWA

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE = 'agentia-v1';
+const ASSETS = ['/agentia/', '/agentia/index.html', '/agentia/manifest.json'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {})));
+  self.skipWaiting();
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+  self.clients.claim();
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+});
